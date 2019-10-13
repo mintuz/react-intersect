bb3223cd36778f49acf35e0547343375be053113
Adam Bulmer <mintuz1990@gmail.com>
2019-10-13 15:46:22 +0100

    React Hook Prototype

--- src/index.js
+++ src/index.js
@@ -1,61 +1,95 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Horizon from '@mintuz/horizon';
+import React, { useState, useCallback } from "react";
+import ReactDOM from "react-dom";
+import Horizon from "@mintuz/horizon";
+
+export const useIntersect = ({
+  triggerOnce = false,
+  intersectionObserverConfig
+}) => {
+  const [inView, setInView] = useState(false);
+
+  const _onEntry = () => {
+    setInView(true);
+  };
+
+  const _onExit = () => {
+    setInView(false);
+  };
+
+  const ref = useCallback(
+    node => {
+      if (node) {
+        Horizon({
+          onEntry: _onEntry,
+          onExit: _onExit,
+          triggerOnce,
+          toObserve: node,
+          intersectionObserverConfig: {
+            ...intersectionObserverConfig
+          }
+        });
+      }
+    },
+    [triggerOnce, intersectionObserverConfig]
+  );
+
+  return [inView, ref];
+};
 
 export default class ReactIntersect extends React.Component {
-    state = {
+  state = {
+    inView: false
+  };
+
+  onEntry = entry => {
+    if (this.props.onEntry) {
+      this.props.onEntry(entry);
+    }
+
+    this.setState(prevState => {
+      const newState = {
+        inView: true
+      };
+
+      return this.props.stateReducer
+        ? this.props.stateReducer(prevState, newState)
+        : newState;
+    });
+  };
+
+  onExit = entry => {
+    if (this.props.onExit) {
+      this.props.onExit(entry);
+    }
+
+    this.setState(prevState => {
+      const newState = {
         inView: false
-    };
+      };
 
-    onEntry = (entry) => {
-        if (this.props.onEntry) {
-            this.props.onEntry(entry);
-        }
+      return this.props.stateReducer
+        ? this.props.stateReducer(prevState, newState)
+        : newState;
+    });
+  };
 
-        this.setState((prevState) => {
-            const newState = {
-                inView: true
-            };
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.inView !== this.state.inView;
+  }
 
-            return this.props.stateReducer
-                ? this.props.stateReducer(prevState, newState)
-                : newState;
-        });
-    };
+  componentDidMount() {
+    Horizon({
+      onEntry: this.onEntry,
+      onExit: this.onExit,
+      triggerOnce: this.props.triggerOnce,
+      toObserve: ReactDOM.findDOMNode(this),
+      intersectionObserverConfig: {
+        ...this.props.intersectionObserverConfig
+      }
+    });
+  }
 
-    onExit = (entry) => {
-        if (this.props.onExit) {
-            this.props.onExit(entry);
-        }
-
-        this.setState((prevState) => {
-            const newState = {
-                inView: false
-            };
-
-            return this.props.stateReducer
-                ? this.props.stateReducer(prevState, newState)
-                : newState;
-        });
-    };
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextState.inView !== this.state.inView;
-    }
-
-    componentDidMount() {
-        Horizon({
-            onEntry: this.onEntry,
-            onExit: this.onExit,
-            triggerOnce: this.props.triggerOnce,
-            toObserve: ReactDOM.findDOMNode(this),
-            intersectionObserverConfig: {
-                ...this.props.intersectionObserverConfig
-            }
-        });
-    }
-
-    render() {
-        return this.props.render(this.state.inView);
-    }
+  render() {
+    return this.props.render(this.state.inView);
+  }
 }
